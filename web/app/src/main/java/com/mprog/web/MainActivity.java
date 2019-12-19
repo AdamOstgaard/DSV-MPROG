432e2e6be69dc70c78c29c0d2ffdc667134e7d5b
Adam Östgaard <adam.ostgaard@gmail.com>
2019-12-19 20:03:47 +0100

    uppg 4.1.2

---
+++ web/app/src/main/java/com/mprog/web/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mprog.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String WEBVIEW_EXTRAS_URL = "com.mprog.web.URL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onGo(View view){
+        TextView textView = findViewById(R.id.urlTextView);
+
+        String url = textView.getText().toString();
+        Intent intent = new Intent(this, Website.class);
+        intent.putExtra(WEBVIEW_EXTRAS_URL, url);
+
+        startActivity(intent);
+    }
+}
---
+++ web/app/src/main/java/com/mprog/web/Website.java
@@ -0,0 +1,22 @@
+package com.mprog.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Website extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        String url = getIntent().getStringExtra(MainActivity.WEBVIEW_EXTRAS_URL);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+}
